ffc33946685877c1e37a8d83d82cfa60e986ddc3
Hazemhi <90246438+Hazemhi@users.noreply.github.com>
2021-09-12 23:18:28 +0200

    Create knapsack.cpp
    Added the main function

---
+++ knapsack.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+Using namespace std;
+
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    cout << knapSack(W, wt, val, n);
+    return 0;
+}
